b65163694bdf543ca984fa7d86c53f81ca1a6239
Raraph84 <raraph84@gmail.com>
2021-10-23 09:04:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Youtube Video Downloader
+
+1. Install librairies
+- Run `npm install`.
+
+2. Configure
+- Copy file `config.json.example` to `config.json` and fill his fields.
+
+3. Run downloader
+- Run `node index.js`.
+
+Made by Raraph84 with ❤
---
+++ index.js
@@ -0,0 +1,18 @@
+const Fs = require("fs");
+const Ytdl = require("ytdl-core");
+const ProgressBar = require("progress");
+const Config = require("./config.json");
+
+let downloadingBar;
+
+Ytdl.getInfo(Config.videoId).then((info) => {
+    console.log(`Downloading ${info.videoDetails.title}...`);
+    Ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        .on("response", (res) => downloadingBar = new ProgressBar(`${info.videoDetails.title} ${Config.outputFile} :percent [:bar] :etas`, {
+            complete: String.fromCharCode(0x2588),
+            total: parseInt(res.headers["content-length"], 10)
+        }))
+        .on("data", (data) => downloadingBar.tick(data.length))
+        .on("finish", () => console.log("Download complete !"))
+        .pipe(Fs.createWriteStream(Config.outputFile));
+});
