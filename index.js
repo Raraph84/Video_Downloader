d4bcd2c8635d0364095a3128bbf44135a5107b60
unknown <raraph84@gmail.com>
2022-02-01 21:49:17 +0100

    Fixs

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const Ytdl = require("ytdl-core");
 const ProgressBar = require("progress");
 const Config = require("./config.json");
 
-let downloadingBar;
+const output = Config.output.toLowerCase();
+if (output !== "mp3" && output !== "mp4") {
+    console.log("Output must be mp3 or mp4 !");
+    return;
+}
 
-Ytdl.getInfo(Config.videoId).then((info) => {
-    console.log(`Downloading ${info.videoDetails.title}...`);
-    Ytdl("https://www.youtube.com/watch?v=" + Config.videoId, { quality: "highest" })
-        .on("response", (res) => downloadingBar = new ProgressBar(`${info.videoDetails.title} ${Config.outputFile} :percent [:bar] :etas`, {
+let downloadingBar;
+Ytdl.getInfo(Config.video).then((info) => {
+    const outputfile = info.videoDetails.title.replace(/[ ]/g, "-").replace(/[^a-zA-Z0-9_-]/g, "") + "." + output;
+    console.log(`Downloading ${outputfile}...`);
+    Ytdl(Config.video, { filter: output === "mp3" ? "audioonly" : "video" })
+        .on("response", (res) => downloadingBar = new ProgressBar(`${info.videoDetails.title} ${outputfile} [:bar] :percent :etas`, {
             complete: String.fromCharCode(0x2588),
             total: parseInt(res.headers["content-length"], 10)
         }))
         .on("data", (data) => downloadingBar.tick(data.length))
         .on("finish", () => console.log("Download complete !"))
-        .pipe(Fs.createWriteStream(Config.outputFile));
+        .pipe(Fs.createWriteStream(outputfile));
 });
